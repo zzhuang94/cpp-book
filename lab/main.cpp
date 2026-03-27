e1d80063369921157cb40236856deec888ceb157
zzhuang94 <zzhuang94@sina.com>
2026-03-27 16:57:32 +0800

    optimize

--- lab/main.cpp
+++ lab/main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
-#include <string>
+
+void f(int) {
+    std::cout << "int" << std::endl;
+}
+
+void f(int*) {
+    std::cout << "pointer" << std::endl;
+}
 
 int main() {
-    int count{10};
-    long long total = 9000000000LL;
-    double ratio = 0.75;
-    bool enabled = true;
-    char level = 'A';
-    std::string name = "cpp11";
-
-    std::cout << "count = " << count << std::endl;
-    std::cout << "total = " << total << std::endl;
-    std::cout << "ratio = " << ratio << std::endl;
-    std::cout << "enabled = " << enabled << std::endl;
-    std::cout << "level = " << level << std::endl;
-    std::cout << "name = " << name << std::endl;
+    f(nullptr);
+    f(0);
+    // f(NULL); 编译失败 call of overloaded 'f(NULL)' is ambiguous
     return 0;
 }
